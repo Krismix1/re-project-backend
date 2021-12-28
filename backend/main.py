22ab1300366b4e4594137e9c4d3af887d309d78c
Cristi B <cbetivu@gmail.com>
2021-12-28 12:03:19 +0100

    Remove custom openapi

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,5 @@
 """Main module for FastAPI with creation of the FastAPI app itself."""
-
 from fastapi import FastAPI
-from fastapi.openapi.utils import get_openapi
 
 from backend.router import ops_router, router
 
@@ -10,28 +8,7 @@ OPEN_API_TAGS_METADATA = [
 ]
 
 
-app = FastAPI(title="Internship platform API")
+app = FastAPI(title="Internship platform API", version="0.1.0", openapi_tags=OPEN_API_TAGS_METADATA)
 
 app.include_router(router, prefix="/api/v1")
 app.include_router(ops_router)
-
-
-def custom_openapi():
-    """Custom OpenAPI wrapper as seen in FastAPI docs.
-
-    See: https://fastapi.tiangolo.com/advanced/extending-openapi/
-    """
-    if app.openapi_schema:
-        return app.openapi_schema
-
-    openapi_schema = get_openapi(
-        title=app.title,
-        version="0.1.0",
-        routes=app.routes,
-        tags=OPEN_API_TAGS_METADATA,
-    )
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-
-app.openapi = custom_openapi
