c0c8166a89cc4266690f39ea7f9717634bc0ea93
Cristi <cbetivu@gmail.com>
2022-01-08 15:14:08 +0200

    Basic internship endpoints (#9)

--- backend/core/security.py
+++ backend/core/security.py
@@ -3,17 +3,17 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
+from backend import models
 from backend.core.config import SETTINGS
 from backend.dependencies import get_db
 from backend.schemas.user import User
+from backend.services.company import get_company
 from backend.services.user import get_user_by_email
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/token")
 
 
-async def get_current_user(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
-) -> User:
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -42,3 +42,15 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     if not current_user.is_active:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
     return current_user
+
+
+def get_company_user(
+    current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)
+) -> models.Company:
+    company = get_company(db, current_user.id)
+    if not company:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Only companies can create internships"
+        )
+
+    return company
--- backend/models/company.py
+++ backend/models/company.py
@@ -1,3 +1,5 @@
+import uuid
+
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
@@ -21,3 +23,22 @@ class Company(Base):
     phone = Column(String, nullable=False)
 
     account = relationship("User")
+    internships = relationship("Internship", back_populates="company")
+
+
+class Internship(Base):
+    __tablename__ = "internships"
+
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        index=True,
+        nullable=False,
+        unique=True,
+        default=uuid.uuid4,
+    )
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+
+    company_id = Column(UUID(as_uuid=True), ForeignKey("companies.id"))
+    company = relationship("Company", back_populates="internships")
